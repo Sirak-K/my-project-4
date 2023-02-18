545ba26ae53e05154d817dae309791aac1cd10d3
Sirak-K <ssirak@hotmail.com>
2023-02-18 14:31:04 +0100

    Test: Nav Bar

--- socialize/settings.py
+++ socialize/settings.py
@@ -76,12 +76,22 @@ WSGI_APPLICATION = 'socialize.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'P4_DATABASE',
+#         'USER': 'SIRAK_DB',
+#         'PASSWORD': 'Pericolo123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
