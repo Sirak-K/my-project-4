201db44e55b10c073339aa74a640a0d8b2d551de
Sirak-K <ssirak@hotmail.com>
2023-05-23 17:40:10 +0200

    Convert Function-Based Views to Class-Based Views

--- core/urls.py
+++ core/urls.py
@@ -6,34 +6,35 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import UserSearchView, UserProfileView, FriendAcceptView, FriendRejectView, FriendCancelView, FriendListView, FriendRequestView, CommentCreateView, PostDeleteView, PostCreateView, PostListView, PostDetailsView
+from .views import UserProfileImageView, UserProfileFieldUpdateView, PostLikeToggleView, UserFeedView, LogoutView, LoginView, SignUpView, UserSearchView, UserProfileView, FriendAcceptView, FriendRejectView, FriendCancelView, FriendListView, FriendRequestView, CommentCreateView, PostDeleteView, PostCreateView, PostListView, PostDetailsView
 
 
 
 urlpatterns = [
     
-    path('', views.user_feed, name='home'),
+     path('', UserFeedView.as_view(), name='user_feed'),
 
   
-    path('signup/', views.signup_view, name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
     # URLS - LOG-IN / LOG-OUT
-    path('logout/', views.logout_view, name='logout'),
-    path('login/', views.login_view, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # URLS - USER
-    path('user_feed/', views.user_feed, name='user_feed'),
+    path('user_feed/', UserFeedView.as_view(), name='user_feed'),
 
-    # path('user_profile/', UserProfileView.as_view(), name='user_profile'),
+    path('user_profile_image/', UserProfileImageView.as_view(), name='user_profile_image'),
+    path('user_profile_field_update/<int:user_id>/', UserProfileFieldUpdateView.as_view(), name='user_profile_field_update'),
+    
+
+
     path('user_profile/<str:username>/', UserProfileView.as_view(), name='user_profile'),
     
     path('user_search/', UserSearchView.as_view(), name='user_search'),
     
-  
 
-    path('user_profile_image/', views.user_profile_image, name='user_profile_image'),
-    path('user_profile_field_update/<int:user_id>/', views.user_profile_field_update, name='user_profile_field_update'),
-    
-    
-    
+
+
+
     # URLS - Friendship
     path('friend_list/', FriendListView.as_view(), name='friend_list'),
     path('friend_request/', FriendRequestView.as_view(), name='friend_request'),
@@ -46,7 +47,7 @@ urlpatterns = [
 
 
     # URLS - POST
-    path('post_details/toggle-like/<int:pk>/', views.toggle_like, name='toggle_like'),
+    path('post_details/toggle-like/<int:pk>/', PostLikeToggleView.as_view(), name='toggle_like'),
     path('post_details/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
     path('post_details_page/<int:pk>/', PostDetailsView.as_view(), name='post_details_page'),
     path('post_details', PostDetailsView.as_view(), name='post_details'),
--- core/views.py
+++ core/views.py
@@ -14,85 +14,95 @@ from django.urls import reverse_lazy, reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST, require_http_methods
 
-from django.views.generic import TemplateView, View, UpdateView, DeleteView, CreateView, ListView, DetailView
+from django.views.generic import RedirectView, TemplateView, FormView, View, UpdateView, DeleteView, CreateView, ListView, DetailView
 from .forms import UserSearchForm, CommentForm, PostForm, PostEditForm, UpdateProfileForm, UpdateProfileImageForm
 from .models import models, User, Friendship, Comment, Post, Profile
 
+# VIEW 0 - AUTHENTICATION
+# - API-Endpoint
+class LoginRequiredMixin:
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+# VIEW 1 - SIGNUP
+# - POST, REDIRECT
+class SignUpView(FormView):
+    template_name = 'signup.html'
+    form_class = UserCreationForm
+    success_url = 'user_feed'
 
-# VIEW 1 - SIGN-UP
-def signup_view(request):
-    # Check if the request is a POST request
-    if request.method == 'POST':
-        # Create a user creation form with the POST data
-        form = UserCreationForm(request.POST)
-        # Check if the form is valid
-        if form.is_valid():
-            # Save the user to the database
-            user = form.save()
-            # Log the user in
-            login(request, user)
-            # Redirect the user to the user_feed page
-            return redirect('user_feed') 
-    else:
-        # If it's not a POST request, create an empty form
-        form = UserCreationForm()
-    # Render the signup template with the form
-    return render(request, 'signup.html', {'form': form})
-# VIEW 2 - LOG-IN
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('user_feed')
-    else:
-        if request.method == 'POST':
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('user_feed')
-            else:
-                messages.error(request, 'The provided username or password is incorrect.')
-                return render(request, 'login.html')
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+# VIEW 2 - LOGIN
+# - GET, POST, REDIRECT
+class LoginView(View):
+    template_name = 'login.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('user_feed')
         else:
-            logout(request) 
-            return render(request, 'login.html')
-# VIEW 3 - [ LOG-OUT ] 
-def logout_view(request):
-    logout(request)
-    return redirect('login')
-# VIEW 4 - HOME [ LOGGED-IN ] --------->
-@login_required
-def index(request):
-   
-    return render(request, 'user_feed.html')
+            logout(request)
+            return render(request, self.template_name)
 
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('user_feed')
+        else:
+            messages.error(request, 'The provided username or password is incorrect.')
+            return render(request, self.template_name)   
+# VIEW 3 - LOGOUT
+# - REDIRECT
+class LogoutView(RedirectView):
+    url = 'login'
+
+    def get_redirect_url(self, *args, **kwargs):
+        logout(self.request)
+        return super().get_redirect_url(*args, **kwargs)
+# VIEW 4 - INDEX
+# - GET, HTML/TEMPLATE
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'user_feed.html'
 # VIEW 5 - USER FEED (displays posts)
-@login_required
-def user_feed(request):
-    user = request.user
-    friend_ids = Friendship.objects.filter(
-        Q(sender=user, status='accepted') | Q(receiver=user, status='accepted')
-    ).values_list('sender_id', 'receiver_id')
-
-    # Include the logged-in user's own posts
-    friend_ids = [friend_id for friend_id in friend_ids for friend_id in friend_id]
-    friend_ids.append(user.id)
-
-    all_posts = Post.objects.filter(post_author_id__in=friend_ids).order_by('-post_created_at')
-    all_post_comments = Comment.objects.all().order_by('-comment_created_at')
-
-    return render(request, 'user_feed.html', {'all_posts': all_posts, 'all_post_comments': all_post_comments})
+# - GET, HTML/TEMPLATE
+class UserFeedView(LoginRequiredMixin, ListView):
+    template_name = 'user_feed.html'
+    context_object_name = 'all_posts'
+    paginate_by = 10
 
 
+    def get_queryset(self):
+        user = self.request.user
+        friend_ids = Friendship.objects.filter(
+            Q(sender=user, status='accepted') | Q(receiver=user, status='accepted')
+        ).values_list('sender_id', 'receiver_id')
 
+        # Include the logged-in user's own posts
+        friend_ids = [friend_id for friend_id in friend_ids for friend_id in friend_id]
+        friend_ids.append(user.id)
 
+        return Post.objects.filter(post_author_id__in=friend_ids).order_by('-post_created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_post_comments'] = Comment.objects.all().order_by('-comment_created_at')
+        return context
 # VIEW 6 - USER PROFILE (also displays posts)
+# - GET, POST, HTML/TEMPLATE
 class UserProfileView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = UpdateProfileForm
     template_name = 'user_profile.html'
     
-    # Update the get_object method to accept a username from the URL
     def get_object(self, queryset=None):
         username = self.kwargs['username']
         user = get_object_or_404(User, username=username)
@@ -135,25 +145,8 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         messages.success(self.request, 'Profile updated successfully.')
         return super().form_valid(form)
-
-
-
-
-   
-   
-   
-    # Retrieve search query from request body
-
-# data: stores the result of parsing the request body as JSON data.
-# search_query: stores the value of the 'search_query' key from the parsed JSON data. It represents the search query input by the user.
-# json.loads(): converts the JSON-formatted request body into a Python object.
-# request.body: the raw request body that contains the data sent by the client.
-    
-# The get method is responsible for handling HTTP GET requests, 
-# which are commonly used for retrieving and displaying information. 
-
-
-# VIEW 6 - USER SEARCH 
+# VIEW 7 - USER SEARCH
+# - GET, POST, JSON
 class UserSearchView(View):
     template_name = 'user_search.html'
 
@@ -181,59 +174,52 @@ class UserSearchView(View):
             'users': list(searched_users.values()),
         }
         return JsonResponse(response_data)
+# VIEW 8 - USER PROFILE - UPLOAD IMAGES
+# - GET, POST, REDIRECT
+class UserProfileImageView(LoginRequiredMixin, FormView):
+    template_name = 'user_profile_image.html'
+    form_class = UpdateProfileImageForm
+    success_url = 'user_feed'
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.request.user.profile
+        return kwargs
 
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Profile image and banner updated successfully.')
+        # Store the updated user profile image URL in the session
+        user_profile = self.request.user.profile
+        self.request.session['user_profile_image_url'] = user_profile.profile_image.url if user_profile.profile_image else None
+        return super().form_valid(form)
+# VIEW 9 - USER PROFILE - UPDATE PROFILE
+# - POST, JSON
+class UserProfileFieldUpdateView(LoginRequiredMixin, View):
+    @require_POST
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
+    def post(self, request, user_id):
+        try:
+            data = json.loads(request.body)
+            print(f"Received data: {data}")
 
-
-
-
-# VIEW 7 - LOGGED-IN - PROFILE - UPLOAD IMAGES
-@login_required
-def user_profile_image(request):
-    user_profile = request.user.profile
-
-    if request.method == 'POST':
-        form = UpdateProfileImageForm(request.POST, request.FILES, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile image and banner updated successfully.')
-            # Store the updated user profile image URL in the session
-            request.session['user_profile_image_url'] = user_profile.profile_image.url if user_profile.profile_image else None
-            return redirect('user_feed')
-    else:
-        form = UpdateProfileImageForm(instance=user_profile)
-
-    context = {'form': form}
-    return render(request, 'user_profile_image.html', context)
-
-# VIEW 8 - LOGGED-IN - PROFILE - UPDATE PROFILE DETAILS
-@require_POST
-@login_required
-def user_profile_field_update(request, user_id):
-    try:
-        data = json.loads(request.body)
-        print(f"Received data: {data}")
-
-        field_name = data.get('fieldName')
-        field_value = data.get('value')
-        if field_name in ['bio', 'gender', 'profession']:
-            user_profile = Profile.objects.get(user__id=user_id)
-            setattr(user_profile, field_name, field_value)
-            user_profile.save()
-            return JsonResponse({"status": "success"})
-        else:
-            return HttpResponseBadRequest(f"The field '{field_name}' is not a valid field for updating.")
-    except Exception as e:
-        print(f"Error: {e}")
-        print(traceback.format_exc())
-        return JsonResponse({"status": "error", "message": str(e)})
-
-
-
-
-
-# VIEW  - FRIEND REQUESTS
+            field_name = data.get('fieldName')
+            field_value = data.get('value')
+            if field_name in ['bio', 'gender', 'profession']:
+                user_profile = Profile.objects.get(user__id=user_id)
+                setattr(user_profile, field_name, field_value)
+                user_profile.save()
+                return JsonResponse({"status": "success"})
+            else:
+                return HttpResponseBadRequest(f"The field '{field_name}' is not a valid field for updating.")
+        except Exception as e:
+            print(f"Error: {e}")
+            print(traceback.format_exc())
+            return JsonResponse({"status": "error", "message": str(e)})
+# VIEW 10 - FRIEND REQUESTS
+# - POST, REDIRECT
 class FriendRequestView(CreateView):
     model = Friendship
     fields = []
@@ -245,7 +231,8 @@ class FriendRequestView(CreateView):
         form.instance.sender = self.request.user
         form.instance.receiver = self.get_receiver() 
         return super().form_valid(form)
-# VIEW 10 - FRIEND LIST
+# VIEW 11 - FRIEND LIST
+# - GET, HTML/TEMPLATE
 class FriendListView(ListView):
     model = Friendship
     template_name = 'friend_list.html'
@@ -261,7 +248,8 @@ class FriendListView(ListView):
         context['sent_friend_requests'] = Friendship.objects.filter(sender=user, status='pending')
         context['received_friend_requests'] = Friendship.objects.filter(receiver=user, status='pending')
         return context
-# VIEW 11 - Friendship - ACCEPT
+# VIEW 12 - Friendship - ACCEPT
+# - POST, REDIRECT
 class FriendAcceptView(UpdateView):
     model = Friendship
     template_name = 'friend_accept.html'
@@ -275,7 +263,8 @@ class FriendAcceptView(UpdateView):
 
     def get_success_url(self):
         return reverse('friend_list')
-# VIEW 12 - Friendship - REJECT
+# VIEW 13 - Friendship - REJECT
+# - POST, REDIRECT
 class FriendRejectView(UpdateView):
     model = Friendship
     template_name = 'friend_reject.html'
@@ -289,7 +278,8 @@ class FriendRejectView(UpdateView):
 
     def get_success_url(self):
         return reverse('friend_list')
-# VIEW 13 - Friendship - CANCEL
+# VIEW 14 - Friendship - CANCEL
+# - POST, REDIRECT
 class FriendCancelView(UpdateView):
     model = Friendship
     template_name = 'friend_cancel.html'
@@ -303,14 +293,14 @@ class FriendCancelView(UpdateView):
 
     def get_success_url(self):
         return reverse('friend_list')
-
-
-# VIEW 14  - Post List
+# VIEW 15 - Post List
+# - GET, HTML/TEMPLATE
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'post_list_context'
-# VIEW 15  - Post Details
+# VIEW 16 - Post Details
+# - GET, POST, REDIRECT, HTML/TEMPLATE
 class PostDetailsView(DetailView):
     model = Post
     # Byt ej template_name annars kmr inte rendera CSS i single-post views
@@ -351,7 +341,8 @@ class PostDetailsView(DetailView):
         comments = post.comments_for_post.order_by('comment_created_at')
         context['all_post_comments'] = comments
         return context
-# VIEW 16  - Post Create
+# VIEW 17 - Post Create
+# - POST, HTML/TEMPLATE
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
@@ -368,12 +359,33 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.post_author = self.request.user
         return super().form_valid(form)
-# VIEW 17 - Post Delete
+# VIEW 18 - Post Delete
+# - POST, HTML/TEMPLATE
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('user_feed')
-# VIEW 18 - Comment Create
+# VIEW 19 - Post Like
+# - POST, AJAX, JSON
+class PostLikeToggleView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        user = request.user
+
+        if post.is_liked_by_user(user):
+            post.post_likes.remove(user)
+            post_like_status = False
+        else:
+            post.post_likes.add(user)
+            post_like_status = True
+
+        response = {
+            'post_like_status': post_like_status,
+            'like_count': post.post_likes.count()
+        }
+        return JsonResponse(response)
+# VIEW 20 - Comment Create
+# - POST, HTML/TEMPLATE
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
@@ -385,22 +397,6 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         form.instance.comment_on_post = get_object_or_404(Post, pk=self.kwargs['pk'])
         super().form_valid(form)
         return redirect('post_details_page', pk=self.kwargs['pk'])
-# VIEW 19 - Post Like
-@login_required
-def toggle_like(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    user = request.user
 
-    if post.is_liked_by_user(user):
-        post.post_likes.remove(user)
-        post_like_status = False
-    else:
-        post.post_likes.add(user)
-        post_like_status = True
 
-    response = {
-        'post_like_status': post_like_status,
-        'like_count': post.post_likes.count()
-    }
-    return JsonResponse(response)
 
